82c453c17555c0c19d3319e7bc5933f22009053c
Andrew Cobb <andrew@cobbs-web.com>
2019-10-28 21:20:04 -0400

    added activities: recordRoute, shareRoute, FullscreenMap and about

---
+++ app/src/main/java/ca/georgebrown/comp3074/mymovement/RecordRouteActivity.java
@@ -0,0 +1,14 @@
+package ca.georgebrown.comp3074.mymovement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecordRouteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_route);
+    }
+}
---
+++ app/src/main/java/ca/georgebrown/comp3074/mymovement/ShareRouteActivity.java
@@ -0,0 +1,14 @@
+package ca.georgebrown.comp3074.mymovement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShareRouteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_route);
+    }
+}
