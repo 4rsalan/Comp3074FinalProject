ac7f2bc2287b99f0bddfb3ff0fae9b5c06cdae0b
Joey <joeyr699@gmail.com>
2019-11-17 17:14:09 -0500

    "Add Search bar Functionality"

--- app/src/main/java/ca/georgebrown/comp3074/mymovement/ManageRoutesActivity.java
+++ app/src/main/java/ca/georgebrown/comp3074/mymovement/ManageRoutesActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.text.SimpleDateFormat;
@@ -27,10 +29,12 @@ public class ManageRoutesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_routes);
 
-        list = MainActivity.dbHelper.getAllRoutes();
+        MainActivity.dbHelper.getAllRoutes();
 
         //Point variables to UI objects
         ListView listRoutes = findViewById(R.id.listRoutes);
+        final EditText etxtSearchRoute = findViewById(R.id.etxtSearchRoute);
+        Button btnSearch = findViewById(R.id.btnSearch);
 
         //set the List adapater
         adapter = new ListAdapter(this, R.layout.route_item_layout, list);
@@ -49,6 +53,21 @@ public class ManageRoutesActivity extends AppCompatActivity {
                 intent.putExtra("RouteRating", route.getRating());
                 intent.putExtra("RouteTags", route.getTags());
                 startActivityForResult(intent, 1);
+            }
+        });
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String search = etxtSearchRoute.getText().toString();
+                if (search.length() > 0 && search.trim() != "") {
+                    MainActivity.dbHelper.getAllFilteredRoutes(etxtSearchRoute.getText().toString());
+                    adapter.notifyDataSetChanged();
+                }
+                else {
+                    MainActivity.dbHelper.getAllRoutes();
+                    adapter.notifyDataSetChanged();
+                }
             }
         });
     }
